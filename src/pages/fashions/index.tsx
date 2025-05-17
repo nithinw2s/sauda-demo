0c11a4c0feeab03c8a606e49dd653b678da1ee55
Praveen <praveenk@way2smile.com>
2025-05-17 13:12:16 +0530

    Added suggested components

---
+++ src/pages/fashions/index.tsx
@@ -0,0 +1,8 @@
+const MobileComponet = () => {
+    return(
+        <div className="flex flex-col items-center justify-center h-screen">
+        <h1>Fashion Component</h1>
+        </div>
+    )
+}
+export default MobileComponet;
